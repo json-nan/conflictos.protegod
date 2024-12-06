97f5f162563992a398295ea1ebe63b70b626db64
Json <jasson_alex99@hotmail.com>
2024-12-06 17:45:01 -0600

    :sparkles: pati

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ import "./App.css";
 function App() {
   const [lastConflict] = useState(new Date(2024, 11, 6, 16, 15, 0));
   const [daysWithoutConflict, setDaysWithoutConflict] = useState(0);
+  const [showImage, setShowImage] = useState(false);
 
   useEffect(() => {
     setDaysWithoutConflict(dayjs().diff(dayjs(lastConflict), "day"));
   }, []);
 
   return (
-    <>
+    <div className="space-y-4">
       <div>
         <p className="text-6xl font-bold">Contador de días sin conflictos</p>
       </div>
       <div>
-        <p className="text-8xl mt-4">
+        <p className="text-8xl">
           <span
             className={
               daysWithoutConflict > 0 ? "text-green-500" : "text-red-500"
@@ -26,7 +27,19 @@ function App() {
           </span>
         </p>
       </div>
-    </>
+      <button
+        onClick={() => setShowImage(true)}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Si eres la Cuack da clic aquí para tu skin
+      </button>
+      {showImage && (
+        <div className="flex flex-col justify-center items-center">
+          <p>pati</p>
+          <img src="/pati-brenda.jpeg" className="size-20" />
+        </div>
+      )}
+    </div>
   );
 }
 
